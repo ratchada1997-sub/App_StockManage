fbb0db119398b070d2f7a2559ed443191af60db3
ratchada <ratchada.s@xten-technology.com>
2024-03-04 21:22:39 +0700

    commit 2

--- src/screens/testtus/test2.js
+++ src/screens/testtus/test2.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function test() {
+export default function test2() {
   return (
     <View>
-      <Text>tesjjjjjjjjt</Text>
+      <Text>test2</Text>
     </View>
   )
 }
